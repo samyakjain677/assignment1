271b580645850bc480d4d7cf62cbf2e05ae72e0a
samyakjain677 <44441096+samyakjain677@users.noreply.github.com>
2018-10-26 21:21:29 +0530

    Update f1.py
    So far we have fetched the id but company name can't be placed in the field.

--- f1.py
+++ f1.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+
 url = 'http://www.mca.gov.in/mcafoportal/showCheckLLPName.do'
 
 agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
